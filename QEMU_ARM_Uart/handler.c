f02d668cea2b929445e289933f6ff17b3ca09be0
LucasMazocco51 <94092730+LucasMazocco51@users.noreply.github.com>
2023-10-22 17:02:06 -0300

    irq cada 10ms
    implementado timer interrupt a cada 10ms

--- QEMU_ARM_Uart/handler.c
+++ QEMU_ARM_Uart/handler.c
@@ -1,13 +1,61 @@
 #include "stdio.h"
 #include "print.h"
+#include "lib.h"
+#include "irq.h"
 
+void enable_timer(void);
+uint32_t read_timer_status(void);
+void set_timer_interval(uint32_t value);
+uint32_t read_timer_freq(void);
+
+static uint32_t timer_interval = 0;
+static uint64_t ticks = 0;
+
+void init_timer(void)
+{
+    timer_interval = read_timer_freq() / 100;
+    enable_timer();
+    out_word(CNTP_EL0, (1<< 1));
+}
+
+static void timer_interrupt_handler(void)
+{
+    uint32_t status = read_timer_status();
+
+    if (status & (1 << 2))  //interrupt is fired
+    {
+        ticks++;
+        if (ticks % 100 == 0)
+        {
+            printk("timer %d \r\n",ticks);
+        }
+        set_timer_interval(timer_interval);
+    }
+
+
+}
 void handler(uint64_t numid, uint64_t esr, uint64_t elr)
 {
+    uint32_t irq;
+
     switch (numid)
     {
     case 1:                                 //só trata o erro de sync
         printk("sync error at %x: %x\r\n", elr,esr);
         while (1){}
+        break;
+    
+
+    case 2:                                 //trata a interrupção do timer
+        irq = in_word(CNTP_STATUS_EL0);
+        if (irq & (1<<1))                   //verifica se o bit da interrupção está ativo
+        {
+            timer_interrupt_handler();      //chama a função de interrup
+        }
+        else
+        {
+            printk("unknow irq\r\n");
+        }
         break;
     
     default:
--- QEMU_ARM_Uart/main.c
+++ QEMU_ARM_Uart/main.c
@@ -2,6 +2,8 @@
 #include "print.h"
 #include "debug.h"
 #include "lib.h"
+#include "handler.h"
+
 
 void KMain(void)
 {
@@ -12,7 +14,8 @@ void KMain(void)
     printk("Hello, Rasp\r\n");
     printk("We are at EL %u\r\n", (uint64_t)get_el());
 
-
+    init_timer();
+    enable_irq();
 
     while (1) {
         ;
